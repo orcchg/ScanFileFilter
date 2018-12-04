9638ced6e97293fe3096fe2fe671e0889614da0f
Maxim Alov <maxim@aludio.com>
2018-12-04 18:00:39 +0300

    Control flow with buttons

--- app/src/main/java/com/orcchg/scanfilefilter/MainActivity.java
+++ app/src/main/java/com/orcchg/scanfilefilter/MainActivity.java
@@ -11,6 +11,10 @@ import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button buttonStart;
+    private Button buttonReset;
+    private EditText etUrl;
+    private EditText etRegexp;
     private ListView list;
 
     @Override
@@ -18,14 +22,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final EditText etUrl = findViewById(R.id.et_url);
-        final EditText etRegexp = findViewById(R.id.et_regexp);
-        Button buttonStart = findViewById(R.id.btn_start);
+        etUrl = findViewById(R.id.et_url);
+        etRegexp = findViewById(R.id.et_regexp);
+        buttonStart = findViewById(R.id.btn_start);
+        buttonReset = findViewById(R.id.btn_reset);
         list = findViewById(R.id.lv_items);
 
         buttonStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Check for validity.
                 String url = etUrl.getText().toString();
                 if (url.isEmpty() || !URLUtil.isValidUrl(url)) {
                     etUrl.setError("Invalid URL!");
@@ -37,12 +43,39 @@ public class MainActivity extends AppCompatActivity {
                     return;
                 }
 
+                hideViews(true);
                 start(url, regexp);
+            }
+        });
+
+        buttonReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stop();
+                hideViews(false);
             }
         });
     }
 
     private void start(String url, String regexp) {
         //
+    }
+
+    private void stop() {
+        // TODO
+    }
+
+    private void hideViews(Boolean isHide) {
+        if (isHide) {
+            buttonStart.setVisibility(View.GONE);
+            buttonReset.setVisibility(View.VISIBLE);
+            etUrl.setVisibility(View.GONE);
+            etRegexp.setVisibility(View.GONE);
+        } else {
+            buttonStart.setVisibility(View.VISIBLE);
+            buttonReset.setVisibility(View.GONE);
+            etUrl.setVisibility(View.VISIBLE);
+            etRegexp.setVisibility(View.VISIBLE);
+        }
     }
 }
